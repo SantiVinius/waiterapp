d874fdc5d995c6a05611c297d37aa7f523b98dba
Vínius Santi <backup.vinius@gmail.com>
2023-04-05 20:14:53 -0300

    app mobile v1 finish

---
+++ app/src/components/Menu/index.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { Product } from '../../types/Product';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { PlusCircle } from '../Icons/PlusCircle';
+import { ProductModal } from '../ProductModal';
+import { Text } from '../Text';
+
+import {
+  AddToCartButton,
+  ProductImage,
+  ProductDetails,
+  Separator,
+  ProductContainer,
+} from './styles';
+
+interface MenuProps {
+  onAddToCart: (product: Product) => void;
+  products:Product[];
+}
+
+export function Menu({ onAddToCart, products }: MenuProps) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<null | Product>(null);
+
+  function handleOpenModal(product: Product) {
+    setIsModalVisible(true);
+    setSelectedProduct(product);
+  }
+
+  return (
+    <>
+      <ProductModal
+        visible={isModalVisible}
+        product={selectedProduct}
+        onClose={() => setIsModalVisible(false)}
+        onAddToCart={onAddToCart}
+      />
+
+      <FlatList
+        data={products}
+        style={{ marginTop: 32 }}
+        contentContainerStyle={{ paddingHorizontal: 24 }}
+        keyExtractor={(product) => product._id}
+        ItemSeparatorComponent={Separator}
+        renderItem={({ item: product }) => (
+          <ProductContainer onPress={() => handleOpenModal(product)}>
+            <ProductImage
+              source={{
+                uri: `http://192.168.15.42:3001/uploads/${product.imagePath}`,
+              }}
+            />
+            <ProductDetails>
+              <Text weight="600">{product.name}</Text>
+              <Text size={14} color="#666" style={{ marginVertical: 8 }}>
+                {product.description}
+              </Text>
+              <Text size={14} weight="600">
+                {formatCurrency(product.price)}
+              </Text>
+            </ProductDetails>
+
+            <AddToCartButton onPress={() => onAddToCart(product)}>
+              <PlusCircle />
+            </AddToCartButton>
+          </ProductContainer>
+        )}
+      />
+    </>
+  );
+}
